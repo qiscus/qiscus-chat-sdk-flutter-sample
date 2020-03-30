7693db63f263c9ef6c0a9c0db6edd5fb456f0271
Afief Sholichuddin <apiep@users.noreply.github.com>
2020-03-30 16:49:05 +0700

    Working online status

---
+++ lib/src/page/rooms_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoomListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RoomListState();
+}
+
+class _RoomListState extends State<RoomListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
